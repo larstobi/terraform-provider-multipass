537f873ff4ddcfa7226a123bfbccedb39dc6cead
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2023-05-31 14:26:36 +0200

    Formatting only

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,20 +1,20 @@
 package provider
 
 import (
-    "context"
-    "fmt"
-    "os"
+	"context"
+	"fmt"
+	"os"
 
-    "github.com/hashicorp/terraform-plugin-framework/diag"
-    "github.com/hashicorp/terraform-plugin-framework/tfsdk"
-    // "github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	// "github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var stderr = os.Stderr
 
 type provider struct {
-    configured bool
-    version    string
+	configured bool
+	version    string
 }
 
 // type providerData struct {
@@ -22,35 +22,35 @@ type provider struct {
 // }
 
 func (p *provider) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
-    return tfsdk.Schema{
-        Attributes: map[string]tfsdk.Attribute{},
-    }, nil
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{},
+	}, nil
 }
 
 type providerData struct{}
 
 func (p *provider) Configure(ctx context.Context, req tfsdk.ConfigureProviderRequest, resp *tfsdk.ConfigureProviderResponse) {
-    // Retrieve provider data from configuration
-    var data providerData
-    diags := req.Config.Get(ctx, &data)
-    resp.Diagnostics.Append(diags...)
-    if resp.Diagnostics.HasError() {
-        return
-    }
+	// Retrieve provider data from configuration
+	var data providerData
+	diags := req.Config.Get(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
-    p.configured = true
+	p.configured = true
 }
 
 func (p *provider) GetResources(_ context.Context) (map[string]tfsdk.ResourceType, diag.Diagnostics) {
-    return map[string]tfsdk.ResourceType{
-        "multipass_instance": instanceResourceType{},
-    }, nil
+	return map[string]tfsdk.ResourceType{
+		"multipass_instance": instanceResourceType{},
+	}, nil
 }
 
 func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourceType, diag.Diagnostics) {
-    return map[string]tfsdk.DataSourceType{
-        "multipass_instance": instanceDataSourceType{},
-    }, nil
+	return map[string]tfsdk.DataSourceType{
+		"multipass_instance": instanceDataSourceType{},
+	}, nil
 
 }
 
@@ -67,33 +67,33 @@ func (p *provider) GetDataSources(_ context.Context) (map[string]tfsdk.DataSourc
 // }
 
 func New(version string) func() tfsdk.Provider {
-    return func() tfsdk.Provider {
-        return &provider{
-            version: version,
-        }
-    }
+	return func() tfsdk.Provider {
+		return &provider{
+			version: version,
+		}
+	}
 }
 
 func convertProviderType(in tfsdk.Provider) (provider, diag.Diagnostics) {
-    var diags diag.Diagnostics
+	var diags diag.Diagnostics
 
-    p, ok := in.(*provider)
+	p, ok := in.(*provider)
 
-    if !ok {
-        diags.AddError(
-            "Unexpected Provider Instance Type",
-            fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. This is always a bug in the provider code and should be reported to the provider developers.", p),
-        )
-        return provider{}, diags
-    }
+	if !ok {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			fmt.Sprintf("While creating the data source or resource, an unexpected provider type (%T) was received. This is always a bug in the provider code and should be reported to the provider developers.", p),
+		)
+		return provider{}, diags
+	}
 
-    if p == nil {
-        diags.AddError(
-            "Unexpected Provider Instance Type",
-            "While creating the data source or resource, an unexpected empty provider instance was received. This is always a bug in the provider code and should be reported to the provider developers.",
-        )
-        return provider{}, diags
-    }
+	if p == nil {
+		diags.AddError(
+			"Unexpected Provider Instance Type",
+			"While creating the data source or resource, an unexpected empty provider instance was received. This is always a bug in the provider code and should be reported to the provider developers.",
+		)
+		return provider{}, diags
+	}
 
-    return *p, diags
+	return *p, diags
 }
